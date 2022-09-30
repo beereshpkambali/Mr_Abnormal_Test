9c7ff41526b54c8fae2483ceaf33c6ac205143a7
I_am_Mr_Abnormal <106374780+beereshpkambali@users.noreply.github.com>
2022-09-30 19:34:58 +0530

    Update filters_mdb.py

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -10,9 +10,7 @@ mydb = myclient[DATABASE_NAME]
 
 
 async def add_filter(grp_id, text, reply_text, btn, file, alert):
-    mycol = mydb[str(grp_id)]
-    # mycol.create_index([('text', 'text')])
-
+    mycol = mydb[str(grp_id)] # mycol.create_index([('text', 'text')])
     data = {
         'text':str(text),
         'reply':str(reply_text),
@@ -30,8 +28,7 @@ async def add_filter(grp_id, text, reply_text, btn, file, alert):
 async def find_filter(group_id, name):
     mycol = mydb[str(group_id)]
     
-    query = mycol.find( {"text":name})
-    # query = mycol.find( { "$text": {"$search": name}})
+    query = mycol.find( {"text":name}) # query = mycol.find( { "$text": {"$search": name}})
     try:
         for file in query:
             reply_text = file['reply']
